d92a006aeae18e99f540dd489f147b0a24cb1364
GP97135 <patidargaurav@johndeere.com>
2020-05-29 15:41:14 +0530

    added config file and constant

---
+++ src/actionTypes.js
@@ -0,0 +1,5 @@
+export const SET_CATEGORY = 'SET_CATEGORY';
+export const SET_CART = 'SET_CART';
+export const GET_ORDERS = 'GET_ORDERS';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+export const GET_TRACK = 'GET_TRACK';
